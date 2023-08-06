d1db600ec773bc7083a3d5a3ebea72c461877b13
Matthew French <matthewfrenchnelson@gmail.com>
2023-08-06 16:21:39 +1200

    inherit methods from WorkflowGenerator

--- sparc_flow/core/workflow.py
+++ sparc_flow/core/workflow.py
@@ -1,28 +1,9 @@
 from scriptcwl import WorkflowGenerator
 
-
-class Workflow:   
-    def __init__(self, language="cwl"): 
-        if language == "cwl":
-            self.wf = WorkflowGenerator()  
+class Workflow(WorkflowGenerator):   
+    def __init__(self, language="cwl"):   
+        super().__init__()
+        if language == "cwl": 
+            pass
         else: 
             raise NotImplementedError("Only CWL is supported at this time.")
-        
-    def __enter__(self): 
-        return self.wf
-    
-    def __exit__(self, exc_type, exc_value, traceback): 
-        pass 
-
-    def load(self, steps_dir): 
-        pass 
-
-    def add_input(self, name, type): 
-        pass 
-
-    def add_outputs(self, name, type): 
-        pass 
-
-    def save(self, filename): 
-        pass 
-        
