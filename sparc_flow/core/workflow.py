7db7babcc75b9b6e9936fc9d64d505b1dea9516a
Matthew French <matthewfrenchnelson@gmail.com>
2023-08-06 15:30:03 +1200

    api skeleton

--- sparc_flow/core/workflow.py
+++ sparc_flow/core/workflow.py
@@ -1,4 +1,28 @@
-class Workflow:  
+from scriptcwl import WorkflowGenerator
 
-    def __init__(self): 
-        pass   
+
+class Workflow:   
+    def __init__(self, language="cwl"): 
+        if language == "cwl":
+            self.wf = WorkflowGenerator()  
+        else: 
+            raise NotImplementedError("Only CWL is supported at this time.")
+        
+    def __enter__(self): 
+        return self.wf
+    
+    def __exit__(self, exc_type, exc_value, traceback): 
+        pass 
+
+    def load(self, steps_dir): 
+        pass 
+
+    def add_input(self, name, type): 
+        pass 
+
+    def add_outputs(self, name, type): 
+        pass 
+
+    def save(self, filename): 
+        pass 
+        
