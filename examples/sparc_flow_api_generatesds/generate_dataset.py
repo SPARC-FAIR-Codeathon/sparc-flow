2a729d70873908233245002822ac516987b162a9
LinkunGao <gaolinkun123@gmail.com>
2023-08-08 13:59:46 +1200

    add comments

--- examples/sparc_flow_api_generatesds/generate_dataset.py
+++ examples/sparc_flow_api_generatesds/generate_dataset.py
@@ -4,6 +4,9 @@ import shutil
 workflow = sparc_flow.Workflow()
 tool = sparc_flow.Tool()
 # shutil.copytree("./examples/sparc_flow_api_example/tools/", "./resources/generated_datasets/tools_dataset/primary/tools")
+
+
+# See example sparc_flow_api_example
 # tool.create_sds("./resources/generated_datasets/tools_dataset", "./examples/sparc_flow_api_example/tools/")
 
 # workflow.create_sds("./resources/generated_datasets/workflow_dataset", "./examples/sparc_flow_api_example/")
