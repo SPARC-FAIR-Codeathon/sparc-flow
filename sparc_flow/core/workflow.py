8919ebb6857a0860b52a7bd90cc0bdf9a45ee02d
Matthew French <matthewfrenchnelson@gmail.com>
2023-08-08 12:10:48 +1200

    save workflow CWL file is folder up from tools folder

--- sparc_flow/core/workflow.py
+++ sparc_flow/core/workflow.py
@@ -39,11 +39,16 @@ class Workflow(WorkflowGenerator):
         self.input_value = None 
         self.input_name = None
         self.tool_dir = None   
-        self.tools = None
+        self.tools = None 
+        self.workflow_dir = None
 
-    def set_steps(self, tool_path):  
+    def set_steps(self, tool_path):   
+        # load steps from tool directory
         self.load(steps_dir=tool_path) 
-        self.tool_dir = tool_path  
+        self.tool_dir = tool_path   
+        # use tool directory to determine workflow directory (standise as one folder up from tool directory) 
+        self.workflow_dir = os.path.normpath(tool_path + os.sep + os.pardir) 
+        # create list of tools in tool directory
         tools = [tool for tool in os.listdir(tool_path) if tool.endswith(".cwl")] 
         self.tools = [tool.split(".")[0] for tool in tools] 
 
@@ -64,7 +69,7 @@ class Workflow(WorkflowGenerator):
                                                                 # an attempt is made above, but it doesn't work currently.
 
         self.add_outputs(final_answer=tool_output) 
-        self.save(f'{self.tool_dir}/workflow.cwl', mode='abs')    
+        self.save(f'{self.workflow_dir}/workflow.cwl', mode='abs')    
          
     def run(self, runner="cwltool"):  
 
@@ -77,12 +82,12 @@ class Workflow(WorkflowGenerator):
                             'workflow', 
                             'launch',
                             '--local-entry'
-                        f'{self.tool_dir}/workflow.cwl', 
+                        f'{self.workflow_dir}/workflow.cwl', 
                             '--json',
                         f'{self.tool_dir}/inp_job.json']) 
         else:
             subprocess.run(['cwltool', 
-                            f'{self.tool_dir}/workflow.cwl', 
+                            f'{self.workflow_dir}/workflow.cwl', 
                             f'{self.tool_dir}/inp_job.json']) 
 
 class Tool:  
